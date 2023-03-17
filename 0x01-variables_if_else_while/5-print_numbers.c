85a45d4b17e2192ac43067da1dc0c585095d8d89
Vixs101 <elijahvix695@gmail.com>
2023-03-17 20:56:46 +0000

    printing numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - entry level
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0 ; a < 10 ; a++)
+		printf("%d", a);
+	printf("\n");
+	return (0);
+}
