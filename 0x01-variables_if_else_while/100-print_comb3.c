ce636f40b329fec5904e84ef158d5c9dabc182e7
Vixs101 <elijahvix695@gmail.com>
2023-03-17 22:32:10 +0000

    task

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 1 ; j < 10 ; j++)
+		{
+			if (i < j && i !n j)
+			{
+				putchar(i + '0');
+				putchar(i + '0');
+				if (i+j != 17)
+				{
+					putchar(',');
+				pitchar(' ')
+			}
+			
+		}
+	}
+	putchar('\n');
+	return (o):
+}
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,7 +12,7 @@ int main(void)
 	for (i = 0 ; i < 10 ; i++)
 	{
 		if (i == 9)
-			putchar (i = '0');
+			putchar (i + '0');
 		else
 		{
 			putchar(i + '0');
