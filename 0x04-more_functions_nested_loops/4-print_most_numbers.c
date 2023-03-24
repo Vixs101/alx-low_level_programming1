7f274cccd494a999758291574bc600f90085d6e7
Vixs101 <elijahvix695@gmail.com>
2023-03-24 20:12:45 +0000

    a function that prints numbers from 0 - 9 with exceptions

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers from 0 to 9 with exceptions
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int x = 0;
+
+	for (x = 0; x <= 9; x++)
+	{
+		if (x == 2 || x == 4)
+		{
+			continue;
+		}
+		else
+		{
+		_putchar(x + '0');
+		}
+	}
+	_putchar('\n');
+}
