0f00c51cf1acf3879bb6a7b53dec57b9d5ff19c5
Vixs101 <elijahvix695@gmail.com>
2023-03-31 05:03:28 +0000

    a function that changes lower case to upper case

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * *string_toupper - change lower to uppercase
+ * @str: string to be converted
+ * Return: string
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+	}
+	return (str);
+}
